934c39ca1fd1c384ef0e03cba4ff85572ab2edf9
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2022-10-27 11:23:08 +0530

    fix: ignore other attributes while checking for class

--- src/utils/add-class.ts
+++ src/utils/add-class.ts
@@ -1,5 +1,5 @@
 export function addClass(code: string, classes: string | string[], tag?: string): string {
-	const classRE = new RegExp(`<${tag ?? '\w+'} class="([\\w+-:;\\\/* ]*)"`)
+	const classRE = new RegExp(`<${tag ?? '\w+'}[^>]*class="([\\w+-:;\\\/* ]*)"`)
 	classes = !Array.isArray(classes) ? [classes] : classes
 
 	return code.replace(classRE, (match, previousClasses) => {
